8bab760139da6f86834857e60f06692df23fe78b
Owen Yaggy <owyaggy@icloud.com>
2022-05-26 21:31:07 -0400

    a whole lot

--- app/sketch.js
+++ app/sketch.js
@@ -3,13 +3,26 @@ let dimensions;
 let puck;
 let startX;
 let startY;
+let striker;
+let cpu;
+let fr;
+let goalHeight; // as multiplier of height of rink
+
+/**
+ * Notes:
+ * - Coordinate grid for rink is 17.5 x 10 meters (width x height)
+ * - (0, 0) is the top left corner
+ * - (17.5, 10) is the bottom right corner
+ * - all coordinates are mapped on to a grid of width dimensions[0] and height dimensions[1]
+ */
 
 function setup() {
   // put setup code here
     var cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block');
     background(255);
-    frameRate(60);
+    fr = 60;
+    frameRate(fr);
     middle = [width/2, height/2];
     puck = {
         "x": 17.5 / 2, // x-position
@@ -26,9 +39,18 @@ function setup() {
         "y": 10 / 2,
         "charge": -1 // TODO: accurate charge
     }
+    cpu = {
+        "x": 17.5 - 4,
+        "y": 10 / 2,
+        "xv": 0.1,
+        "yv": 0.3,
+        "charge": -1, // TODO: accurate charge
+        "difficulty": 1 // TODO: implement difficulty
+    }
+    goalHeight = 0.7;
     dimensions = checkRatio();
-    startX = (middle[0] - dimensions[0] / 2) + dimensions[1] / 15;
-    startY = (middle[1] - dimensions[1] / 2) + dimensions[1] / 15;
+    startX = (middle[0] - dimensions[0] / 2) + dimensions[1] / 30 + dimensions[0] / 200;
+    startY = (middle[1] - dimensions[1] / 2) + dimensions[1] / 30 + dimensions[0] / 200;
 }
 
 function draw() {
@@ -38,13 +60,20 @@ function draw() {
     drawInfo(); // draws info at bottom of screen
     dimensions = checkRatio();
     drawRink();
+    drawGoals();
     drawPuck();
     updatePuck();
     drawStriker();
+    drawCPU();
 
-    stroke(255);
+    /*stroke(255);
     strokeWeight(1);
-    line(startX, height / 2, width / 2, height / 2);
+    // line(startX, height / 2, width / 2, height / 2);
+    noFill();
+    ellipseMode(CORNERS);
+    ellipse(startX, startY, width - startX, height - startY);
+    ellipseMode(CENTER);
+    ellipse(width/2, height/2, dimensions[0], dimensions[1]);//*/
 }
 
 function updateBackground() {
@@ -74,10 +103,10 @@ function checkRatio() { // determines which dimension is the limiting factor (wi
     // returns [width, height] of playing field in relation to center
     if (width/(height - 100) >= 1.75) {
         // can expand to max width
-        return [(height-100)*1.75, height-100];
+        return [(height-100)*1.75*.95, (height-100)*.95];
     } else {
         // can expand to max height
-        return [width, width/1.75];
+        return [width*.95, (width/1.75)*.95];
     }
 }
 
@@ -86,7 +115,15 @@ function drawRink() {
     fill(200, 200, 200, 150);
     stroke(255, 0, 0);
     strokeWeight(dimensions[0] / 100);
-    rect(middle[0], middle[1], dimensions[0] - 25, dimensions[1] - 25, 10);
+    rect(middle[0], middle[1], dimensions[0], dimensions[1], 10); // TODO: lower/remove angle at low sizes
+}
+
+function drawGoals() {
+    fill(255, 50, 255, 80);
+    strokeWeight(dimensions[0] / 150);
+    rectMode(CENTER);
+    rect(middle[0] - dimensions[0] / 2 + dimensions[0] / 20, middle[1], dimensions[0] / 10, dimensions[1] * goalHeight, 0, 40, 40, 0);
+    rect(middle[0] + dimensions[0] / 2 - dimensions[0] / 20, middle[1], dimensions[0] / 10, dimensions[1] * goalHeight, 40, 0, 0, 40);
 }
 
 function drawPuck() {
@@ -106,7 +143,6 @@ function updatePuck() {
 }
 
 function drawStriker() {
-    //if (keyIsPressed && keyCode == 32) striker["charge"] *= -1;
     if (striker["charge"] < 0) fill(0, 0, 255);
     else fill(255, 0, 0);
     noStroke();
@@ -120,19 +156,21 @@ function drawStriker() {
         x = mouseX;
     } else {
         cursor(ARROW);
+        x = width - startX;
     }
     if (mouseY < startY) {
         cursor(ARROW);
         y = startY;
     } else if (mouseY >= startY && mouseY <= height - startY) {
-        noCursor();
+        if (mouseX < startX || mouseX > width - startX) cursor(ARROW);
+        else noCursor();
         y = mouseY;
     } else {
         cursor(ARROW);
-        y = startY;
+        y = height - startY;
     }
-    if (mouseY > middle[1] - dimensions[1] / 2) y = mouseY;
     striker["x"] = (x - (width - dimensions[0]) / 2) / dimensions[0] * 17.5;
+    striker["y"] = (y - (height - dimensions[1]) / 2) / dimensions[1] * 10.0;
     ellipse(x, y, dimensions[1] / 15, dimensions[1] / 15);
     fill(255);
     rectMode(CENTER);
@@ -140,9 +178,25 @@ function drawStriker() {
     if (striker["charge"] > 0) rect(x, y, dimensions[1] / 90, dimensions[1] / 22, 10);
 }
 
-function updateStriker() {}
 
-function drawCPU() {}
+function drawCPU() {
+    fill(0, 0, 255);
+    noStroke();
+    cpu["x"] += cpu["xv"] / fr;
+    cpu["y"] += cpu["yv"] / fr;
+    let x = (cpu["x"] / 17.5) * dimensions[0] + (width - dimensions[0]) / 2;
+    let y = (cpu["y"] / 10) * dimensions[1] + (height - dimensions[1]) / 2;
+    if (x < startX) x = startX;
+    if (x > width - startX) x = width - startX;
+    if (y < startY) y = startY;
+    if (y > height - startY) y = height - startY;
+    cpu["x"] = (x - (width - dimensions[0]) / 2) / dimensions[0] * 17.5;
+    cpu["y"] = (y - (height - dimensions[1]) / 2) / dimensions[1] * 10.0;
+    ellipse(x, y, dimensions[1] / 15, dimensions[1] / 15);
+    fill(255);
+    rectMode(CENTER);
+    rect(x, y, dimensions[1] / 22, dimensions[1] / 90, 10);
+}
 
 function updateCPU() {}
 
@@ -153,6 +207,7 @@ function keyPressed() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   middle = [width / 2, height / 2];
-  startX = (middle[0] - dimensions[0] / 2)+ dimensions[1] / 15;
-  startY = (middle[1] - dimensions[1] / 2) + dimensions[1] / 15;
+  dimensions = checkRatio();
+  startX = (middle[0] - dimensions[0] / 2)+ dimensions[1] / 30 + dimensions[0] / 200;
+  startY = (middle[1] - dimensions[1] / 2) + dimensions[1] / 30 + dimensions[0] / 200;
 }
